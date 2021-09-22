644eeb604543ecdc8b88a73d0851e3a642046ea5
Francisco Souza Rangel <83520199+Francisco-S-Rangel@users.noreply.github.com>
2021-09-22 18:22:59 -0300

    Beginning with Project 2
    beginning with html and css part

---
+++ Project_2=QuizApp/script.js
@@ -0,0 +1,42 @@
+let aux = new Date().getFullYear();
+const quizQuestion = [
+    {
+        question: 'What is the most used programmig language used in '+aux+' ?',
+        a: 'C#',
+        b: 'Java',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'd'
+
+    },{
+        question: 'What is the famous mother of others programming languages?',
+        a: 'C++',
+        b: 'C',
+        c: 'Kotlin',
+        d: 'Fortram',
+        correct: 'b'
+    }, {
+        question: 'Where is located New Zeland?',
+        a: 'Oceania',
+        b: 'America',
+        c: 'Europe',
+        d: 'Asia',
+        correct: 'a'
+    }, {
+        question: 'Whom painted the Mona Lisa?',
+        a: 'Leonardo da Vinci',
+        b: 'Vicent van Gogh',
+        c: 'Pablo Picasso',
+        d: 'Michelangelo',
+        correct: 'a'
+    }
+    , {
+        question: 'What does HTML stand for?',
+        a: 'Human toll for Management',
+        b: 'Cascading Style Sheet',
+        c: 'Hypertext Markup Language',
+        d: 'Json Object Notation',
+        correct: 'c'
+    }
+]
+
