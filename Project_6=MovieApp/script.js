ff7fca62603ba470ca55becfab07e32b25237276
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2021-10-07 19:20:32 -0300

    Project 6 is done
    It is simple but it is working as suppossed to be and it has a nice design . The API works as should too.

--- Project_6=MovieApp/script.js
+++ Project_6=MovieApp/script.js
@@ -1,13 +1,20 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 const main = document.querySelector(".main");
+const form = document.getElementById('form');
+const search = document.querySelector('.search');
 
-async function getMovies() {
-    const resp = await fetch(API_URL);
+getMovies(API_URL);
+async function getMovies(url) {
+    const resp = await fetch(url);
     const respData = await resp.json();
-
-    console.log(respData);
-    respData.results.forEach((movie) => {
+   
+    showMovies(respData.results);
+}
+function showMovies(movies){
+    main.innerHTML = '';
+    movies.forEach((movie) => {
         const div_movie = document.createElement('div');
         div_movie.classList.add('movie');
         div_movie.innerHTML = 
@@ -17,11 +24,15 @@ async function getMovies() {
                 <h3 class="title">${movie.title}</h3>
                     <span class="${getClassbyRate(movie.vote_average)}">${movie.vote_average}</span>
             </div>
+            <div class="overview">
+            <h4>Overview: </h4>
+            ${movie.overview}
+            </div>
         `;
 
          main.appendChild(div_movie);
     });
-    return respData;
+
 }
 function getClassbyRate(vote){
     if(vote>=7){
@@ -32,4 +43,12 @@ function getClassbyRate(vote){
         return 'red';
     }
 }
-getMovies();
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const serachTerm = search.value;
+
+    if(serachTerm){
+        getMovies(SEARCH_API+serachTerm);
+        search.value='';
+    }
+});
