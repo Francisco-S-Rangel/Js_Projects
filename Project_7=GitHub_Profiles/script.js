843473e4329a9adfdd20c620a8a1bf5e60eaf116
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2021-10-09 19:35:33 -0300

    Project 7 is done
    Everything is working !!
    Basically you have the description of all profiles on GitHub,of course a simple description.

--- Project_7=GitHub_Profiles/script.js
+++ Project_7=GitHub_Profiles/script.js
@@ -3,35 +3,56 @@ const main = document.querySelector(".main");
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
-async function getUserGithub(user){
-    const resp = await fetch(API_URL+user);
+async function getUserGithub(username){
+    const resp = await fetch(API_URL+username);
     const respData = await resp.json();
 
-    console.log(respData);
     createUserCard(respData);
+    getRepos(username);
+}
+async function getRepos(username){
+    const resp = await fetch(API_URL+username+"/repos");
+    const respData = await resp.json();
 
+    addReposToCard(respData);
 }
 function createUserCard(user){
     const card = document.createElement('div');
     card.classList.add('card');
     const cardHTML = `
     <div class="card">
-    <div>
+    <div class="image-container">
        <img class="avatar" src="${user.avatar_url}" alt="${user.name}"/>
     </di>
     <div class="user-info">
         <h2>${user.name}</h2>
         <p>${user.bio}</p>
         <ul class="ul_info">
-        <li>${user.followers}</li>
-        <li>${user.following}</li>
-        <li>${user.public_repos}</li>
+        <li>${user.followers} - Followers.</li>
+        <li>${user.following} - Following.</li>
+        <li>${user.public_repos} - Public Repositories.</li>
         </ul>
+        <h4>Repositories:</h4>
+        <div class="repos"></div>
     </div>
     </div>
     `;
 
     main.innerHTML = cardHTML;
+}
+function addReposToCard(repos){
+    const resp = document.querySelector(".repos");
+
+    repos.forEach(repo =>{
+        const repoEl = document.createElement('a');
+        repoEl.classList.add('repo');
+        repoEl.href = repo.html_url;
+        repoEl.target = "_blank";
+        repoEl.innerHTML = repo.name;
+
+        resp.appendChild(repoEl);
+    });
+
 }
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
@@ -41,4 +62,3 @@ form.addEventListener("submit",(e)=>{
         search.value = "";
     }
 });
-getUserGithub("Francisco-S-Rangel");
