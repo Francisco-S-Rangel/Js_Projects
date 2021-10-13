fd236cdf88f98e041d900de5b9c8d57bb0cfcbe2
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2021-10-12 22:07:08 -0300

    Project 9 is done
    This project gonna create a random password between 5 to 22 characters and allow the user to copy this password.

---
+++ project_9=Password_Generador/script.js
@@ -0,0 +1,69 @@
+const pwEl = document.getElementById('pw');         
+const copyBtn = document.querySelector('.copy');   
+const lenghtEl = document.getElementById('lenght');   
+const upperEl = document.getElementById('upper');
+const lowerEl = document.getElementById('lower'); 
+const numberEl = document.getElementById('number'); 
+const symbolEl = document.getElementById('symbols'); 
+const generateBtn = document.querySelector('.generate');
+
+const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = '!@#$%&*()-_+=§?/.';
+
+generateBtn.addEventListener('click', generate_password);
+function generate_password(){
+    const len = lenghtEl.value;
+    let password = "";
+
+    for(let i=0; i<len; i++){
+        const x = generateX();
+        password += x;
+    }
+    pwEl.innerText = password;
+}
+function getUppercase(){
+    return upperLetters[Math.floor(Math.random()*upperLetters.length)];
+}
+function getLowercase(){
+    return lowerLetters[Math.floor(Math.random()*lowerLetters.length)];
+}
+function getNumber(){
+    return numbers[Math.floor(Math.random()*numbers.length)];
+}
+function getSymbol(){
+    return symbols[Math.floor(Math.random()*symbols.length)];
+}
+function generateX(){
+    const Xs = [];
+    if(upperEl.checked){
+        Xs.push(getUppercase());
+    }
+    if(lowerEl.checked){
+        Xs.push(getLowercase()); 
+    }
+    if(numberEl.checked){
+        Xs.push(getNumber()); 
+    }
+    if (symbolEl.checked) {
+        Xs.push(getSymbol());
+    }
+    if(Xs.length === 0 ) return " ";
+    return Xs[Math.floor(Math.random() * Xs.length)];
+}
+copyBtn.addEventListener('click',copy_password);
+function copy_password(){
+    const textarea = document.createElement('textarea');
+    const password = pwEl.innerHTML;
+
+    if(!password){return;}
+
+    
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    textarea.remove();
+    alert("Password copied to clipboard");
+}
