077bc2cff69525b6ab465dc2126a6b2392653153
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2021-10-14 19:58:04 -0300

    Project 10 is done
    It is very basic but it is working.
    You can see the weather of every great city(GREAT or FAMOUS) in real time.

--- Project_10=Weather_App/script.js
+++ Project_10=Weather_App/script.js
@@ -1,10 +1,35 @@
 const apikey = "3265874a2c77ae4a04bb96236a642d2f";
 const url =(city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+const main = document.querySelector('.main');
+const form = document.querySelector('.form');
+const search = document.getElementById('search');
 
 async function getWeatherBylocation(city){
     const resp = await fetch(url(city), {origin: "cors"});
     const respData = await resp.json();
 
     console.log(respData);
+    addWeatherToPage(respData);
 }
-getWeatherBylocation('San Francisco');
+function addWeatherToPage(data){
+    const temp = Kelvin_to_Celsius(data.main.temp);
+    const weather = document.createElement('div');
+    weather.classList.add('weather');
+    weather.innerHTML = `
+    <h2> <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />${temp}°C</h2>
+    <small>${data.weather[0].description}</small>   
+    <br>
+    <small class="nameCity">${data.name}</small>
+    `;
+
+    main.innerHTML="";
+    main.appendChild(weather);
+}
+function Kelvin_to_Celsius(K){
+    return Math.floor(K - 273.15);
+}
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    if(search.value){
+    getWeatherBylocation(search.value);}
+});
