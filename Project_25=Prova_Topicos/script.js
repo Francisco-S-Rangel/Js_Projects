5debff10c83624f11a6eb380082c36b04ef6389d
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2022-09-28 23:19:43 -0300

    chegando perto de realizar a compra

--- Project_25=Prova_Topicos/script.js
+++ Project_25=Prova_Topicos/script.js
@@ -53,12 +53,22 @@ quantidadeProduto.innerHTML = '0';
 let contador2=0;
 
 function adicionar(){  
+    if(produtoEscolhido == false){
+        Swal.fire(
+            'Escolha um produto antes!'
+        )
+    }else{
     contador2 ++;
-    quantidadeProduto.innerHTML = contador2;
+    quantidadeProduto.innerHTML = contador2;}
 }
 
 function sub(){
-    if(contador2 <= 0){
+    if(produtoEscolhido == false){
+        Swal.fire(
+            'Escolha um produto antes!'
+        )
+    }
+    else if(contador2 <= 0){
 
     }else{
         contador2 --;
@@ -68,21 +78,43 @@ function sub(){
 
 const produtoSelecionado = document.getElementById("produtoSelecionado");
 produtoSelecionado.innerHTML = '?';
+produtoEscolhido = false;
+produtoAtual=0;
 
 function produto1(){
     produtoSelecionado.innerHTML = 'Barra de Chocolate';
+    produtoEscolhido = true;
+    quantidadeProduto.innerHTML = '0';
+    contador2=0;
+    produtoAtual=1;
 }
 function produto2(){
     produtoSelecionado.innerHTML = 'Salgadinho';
+    produtoEscolhido = true;
+    quantidadeProduto.innerHTML = '0';
+    contador2=0;
+    produtoAtual=2;
 }
 function produto3(){
     produtoSelecionado.innerHTML = 'Refrigerante';
+    produtoEscolhido = true;
+    quantidadeProduto.innerHTML = '0';
+    contador2=0;
+    produtoAtual=3;
 }
 function produto4(){
     produtoSelecionado.innerHTML = 'Bolacha/Biscoito';
+    produtoEscolhido = true;
+    quantidadeProduto.innerHTML = '0';
+    contador2=0;
+    produtoAtual=4;
 }
 function produto5(){
     produtoSelecionado.innerHTML = 'Kinder Ovo';
+    produtoEscolhido = true;
+    quantidadeProduto.innerHTML = '0';
+    contador2=0;
+    produtoAtual=5;
 }
 
 const estoque1 = document.getElementById("estoque1");
@@ -90,8 +122,36 @@ const estoque2 = document.getElementById("estoque2");
 const estoque3 = document.getElementById("estoque3");
 const estoque4 = document.getElementById("estoque4");
 const estoque5 = document.getElementById("estoque5");
-estoque1.innerHTML = 10;
-estoque2.innerHTML = 9;
-estoque3.innerHTML = 12;
-estoque4.innerHTML = 6;
-estoque5.innerHTML = 4;
+produtoestoque1 = 10;
+produtoestoque2 = 9;
+produtoestoque3 = 12;
+produtoestoque4 = 6;
+produtoestoque5 = 4;
+estoque1.innerHTML = produtoestoque1;
+estoque2.innerHTML = produtoestoque2;
+estoque3.innerHTML = produtoestoque3;
+estoque4.innerHTML = produtoestoque4;
+estoque5.innerHTML = produtoestoque5;
+
+function comprar(){
+    if(produtoEscolhido == false){
+        Swal.fire(
+            'Escolha um produto e sua quantidade antes!'
+        )
+    }else if(contador2 == 0){
+        Swal.fire(
+            'Selecione a quantidade do produto antes!'
+        )
+
+    }else if(produtoAtual == 1){
+        let quantidade = parseInt(quantidadeProduto.innerHTML);
+        if(quantidade > produtoestoque1){
+            Swal.fire(
+                'Infelizmente não temos essa quantidade no estoque!'
+            )
+        }else{
+            produtoestoque1 = produtoestoque1 - quantidade;
+            estoque1.innerHTML = produtoestoque1;
+        }
+    }
+}
